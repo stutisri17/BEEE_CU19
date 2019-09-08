2fcae8f13c8fd093b893abd7f3fe090f295206ea
stutisri17 <54950735+stutisri17@users.noreply.github.com>
2019-09-08 18:30:13 +0530

    Add files via upload

---
+++ programs/start_simulating1.ino
@@ -0,0 +1,37 @@
+void setup()
+{
+  for(int i=10;i<=13;i++)
+  {
+    pinMode(i,OUTPUT);
+  }
+}
+
+
+void loop()
+{
+  for(int i=10; i<=13; i++)
+  {
+     allLEDSoff();
+    if(i!=13)
+    {
+     digitalWrite(i, HIGH);
+     digitalWrite(i+1,HIGH); 
+     delay(200);
+    }
+    else
+    {
+     digitalWrite(i,HIGH);
+     digitalWrite(i-3,HIGH);
+      delay(200);
+    }
+  }
+}
+
+ void allLEDSoff()
+ {
+   for(int i=10; i<=13; i++)
+   {
+    digitalWrite(i,LOW);
+   }
+      delay(200);
+ }  
